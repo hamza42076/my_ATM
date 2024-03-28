def7270cda5a599158c564361e518f82b541d799
hamza42076 <164366638+hamza42076@users.noreply.github.com>
2024-03-29 00:30:30 +0500

    This is my third project

---
+++ main.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 10000;
+let pin = 1050;
+let pinNumber = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: "Enter your pincode",
+    }
+]);
+if (pinNumber.pin === pin) {
+    console.log("your pincode is correct!!!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: "please select the Option",
+            choices: ["withdraw", "check balance", "1000"]
+        }
+    ]);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter your amount",
+                type: "number",
+            }
+        ]);
+        myBalance -= amountAns.amount;
+        console.log(`your remaning amount is ${myBalance}`);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log(myBalance);
+    }
+}
+else {
+    console.log("Please try again, becasue your pincode is incorrect! ");
+}
